a773afe3c8825519b156ecc05ae61558b17e93a9
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2022-05-21 11:56:04 +0700

    update

--- 3.3-if/main.go
+++ 3.3-if/main.go
@@ -5,11 +5,13 @@ import "fmt"
 func main() {
 
 	//only use this feature to define new variables that are scoped to the if/else statements; anything else would be confusing.
-	if n := 3; n == 1 {
+	if n := 7; n == 1 {
 		fmt.Println("That's too low", n)
 	} else if n > 5 {
 		fmt.Println("That's too big:", n)
 	} else {
 		fmt.Println("That's a good number:", n)
 	}
+	// n is undeclared
+	// fmt.Println("n:", n)
 }
